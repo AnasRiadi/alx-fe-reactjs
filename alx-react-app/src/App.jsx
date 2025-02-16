3c63be8b759748ed16297e6897ea4bb222d9b057
anas riadi <134967614+AnasRiadi@users.noreply.github.com>
2025-02-16 20:04:24 +0100

    App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
